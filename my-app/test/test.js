83e98cb8be7a755f2011152c4d4e4e2711569376
Reina <reh4fd@virginia.edu>
2020-08-26 05:48:39 -0400

    test name

--- my-app/test/test.js
+++ my-app/test/test.js
@@ -2,11 +2,9 @@ var keystone = require("keystone");
 var utils = require("keystone-utils");
 console.log(utils.isObject({})); // true!
 
-var utils = keystone.utils;
-
 var assert = require("assert");
 describe("Keystone Test", function () {
-  describe("#noop", function () {
+  describe("#notin", function () {
     it("should return true", function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
